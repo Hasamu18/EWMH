a8389559eea39718cd78ef7464a12024d0e8b9f9
Hasamu18 <khoidoan180802@gmail.com>
2024-10-16 11:59:05 +0700

    add product apis

--- Sales/Sales.Api/Controllers/ProductController.cs
+++ Sales/Sales.Api/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Sales.Application.Commands;
+using Sales.Application.Queries;
 using System.Net;
 using static Logger.Utility.Constants;
 
@@ -21,7 +22,7 @@ namespace Sales.Api.Controllers
         }
 
         /// <summary>
-        /// Add a product
+        /// (Manager) Add a product
         /// </summary>
         /// 
         [Authorize(Roles = Role.ManagerRole)]
@@ -36,6 +37,111 @@ namespace Sales.Api.Controllers
                     return BadRequest(result.Item2);
 
                 return Created("", result.Item2);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+                return StatusCode(500, $"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+            }
+        }
+
+        /// <summary>
+        /// (Manager) Update a product
+        /// </summary>
+        /// 
+        [Authorize(Roles = Role.ManagerRole)]
+        [HttpPut("2")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromForm] UpdateProductCommand command)
+        {
+            try
+            {
+                var result = await _mediator.Send(command);
+                if (result.Item1 is 400)
+                    return BadRequest(result.Item2);
+                else if (result.Item1 is 404)
+                    return NotFound(result.Item2);
+
+                return Ok(result.Item2);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+                return StatusCode(500, $"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+            }
+        }
+
+        /// <summary>
+        /// (Manager) Disable a product
+        /// </summary>
+        /// 
+        [Authorize(Roles = Role.ManagerRole)]
+        [HttpPut("3")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DisableProduct([FromForm] DisableProductCommand command)
+        {
+            try
+            {
+                var result = await _mediator.Send(command);
+               if (result.Item1 is 404)
+                    return NotFound(result.Item2);
+
+                return Ok(result.Item2);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+                return StatusCode(500, $"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+            }
+        }
+
+        /// <summary>
+        /// (Authentication) Get a product
+        /// </summary>
+        /// 
+        [Authorize]
+        [HttpGet("4")]
+        [ProducesResponseType(typeof(object), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetProduct([FromQuery] GetProductQuery query)
+        {
+            try
+            {
+                var result = await _mediator.Send(query);
+                if (result.Item1 is 404)
+                    return NotFound(result.Item2);
+
+                return Ok(result.Item2);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+                return StatusCode(500, $"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+            }
+        }
+
+        /// <summary>
+        /// (Authentication) Get all products paginated 
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///     
+        ///     PageIndex       = 1    (default)
+        ///     Pagesize        = 8    (default)
+        ///     SearchByName    = null (default)
+        ///     InOfStock_Sort  = true (default)
+        ///     Status          = null (default)
+        ///   
+        ///     Get all products paginated
+        /// </remarks>
+        [Authorize]
+        [HttpGet("5")]
+        [ProducesResponseType(typeof(object), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetPagedProduct([FromQuery] GetPagedProductQuery query)
+        {
+            try
+            {
+                var result = await _mediator.Send(query);
+                return Ok(result);
             }
             catch (Exception ex)
             {
---
+++ Sales/Sales.Application/Commands/DisableProductCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Application.Commands
+{
+    public class DisableProductCommand : IRequest<(int, string)>
+    {
+        public required string ProductId { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
