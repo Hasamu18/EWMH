b9c848044a48c90c23de2b5b43be1bab19333c22
vathuglife <thuandt582@gmail.com>
2024-11-10 22:22:44 +0700

    dva: added Get Feedback details, and Add feedback endpoint

--- Requests/Requests.Api/Controllers/FeedbackController.cs
+++ Requests/Requests.Api/Controllers/FeedbackController.cs
@@ -22,10 +22,10 @@ namespace Requests.Api.Controllers
             _logger = logger;
         }
         /// <summary>
-        /// (Manager) Get the list of customer feedback in the system.
+        /// (Manager/Customer) Get the list of customer feedback in the system.
         /// </summary>
         /// 
-        [Authorize(Roles = Role.ManagerRole)]
+        [Authorize(Roles = $"{Role.ManagerRole},{Role.CustomerRole}")]
         [HttpGet("1")]
         [ProducesResponseType(typeof(object), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetCustomerFeedbackList(
@@ -40,6 +40,61 @@ namespace Requests.Api.Controllers
                 if (result.Item1 is 404)
                     return NotFound(result.Item2);                
 
+                return Ok(result.Item2);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+                return StatusCode(500, $"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+            }
+        }
+        /// <summary>
+        /// (Manager/Customer) Get the details of a customer feedback in the system.
+        /// </summary>
+        /// 
+        [Authorize(Roles = $"{Role.ManagerRole},{Role.CustomerRole}")]
+        [HttpGet("2")]
+        [ProducesResponseType(typeof(object), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCustomerFeedback(
+            [FromQuery] string feedbackId)
+        {
+            try
+            {                
+                var query = new GetCustomerFeedbackQuery(feedbackId);
+                var result = await _mediator.Send(query);
+                if (result.Item1 is 404)
+                    return NotFound(result.Item2);
+
+                return Ok(result.Item2);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+                return StatusCode(500, $"Error message: {ex.Message}\n\nError{ex.StackTrace}");
+            }
+        }
+        /// <summary>
+        /// (Customer) Allows a Customer to give a feedback to a repair request.
+        /// </summary>
+        /// 
+        [Authorize(Roles = Role.CustomerRole)]
+        [HttpPost("3")]
+        [ProducesResponseType(typeof(object), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateNewCustomerFeedback(
+            [FromBody] CreateNewCustomerFeedbackRequest request)
+        {
+            try
+            {
+                var customerId = (HttpContext.User.FindFirst("accountId")?.Value) ?? "";
+                var query = new CreateNewCustomerFeedbackCommand(request, customerId);
+                var result = await _mediator.Send(query);
+
+                if (result.Item1 is 401)
+                    return Unauthorized(result.Item2);                
+                else if (result.Item1 is 404)
+                    return NotFound(result.Item2);
+                if (result.Item1 is 409)
+                    return Conflict(result.Item2);
                 return Ok(result.Item2);
             }
             catch (Exception ex)
---
+++ Requests/Requests.Application/Queries/GetCustomerFeedbackQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Logger.Utility.Constants;
+
+namespace Requests.Application.Queries
+{
+    public class GetCustomerFeedbackQuery : IRequest<(int,object)>
+    {        
+        public string FeedbackId{ get; set; }        
+        public GetCustomerFeedbackQuery(string feedbackId)
+        {
+            FeedbackId = feedbackId;
+            
+        }
+    }
+}
