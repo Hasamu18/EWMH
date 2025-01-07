4f245e712ea3fc90e4481af007829b62252cffb7
Hasamu18 <khoidoan180802@gmail.com>
2025-01-07 21:15:19 +0700

    add few apis v2

---
+++ Users/Users.Application/Handlers/GetHistoryWorkerByLeaderHandler.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Application.Queries;
+using Users.Domain.IRepositories;
+
+namespace Users.Application.Handlers
+{
+    internal class GetHistoryWorkerByLeaderHandler : IRequestHandler<GetHistoryWorkerByLeaderQuery, object>
+    {
+        private readonly IUnitOfWork _uow;
+        public GetHistoryWorkerByLeaderHandler(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task<object> Handle(GetHistoryWorkerByLeaderQuery request, CancellationToken cancellationToken)
+        {
+            var result = new List<object>();
+
+            var items = (await _uow.WorkerHistoryRepo
+                .GetAsync(a => a.LeaderId.Equals(request.LeaderId)))
+                .ToList();
+
+            var group = items.GroupBy(g => g.WorkerId);
+
+            foreach (var item in group)
+            {
+                var getWorker = await _uow.AccountRepo.GetByIdAsync(item.Key);
+
+                var workerResult = new
+                {
+                    getWorker!.AccountId,
+                    getWorker.FullName,
+                    getWorker.Email,
+                    getWorker.PhoneNumber,
+                    getWorker.AvatarUrl,
+                    getWorker.DateOfBirth,
+                    History = item
+                        .OrderByDescending(h => h.To == null) 
+                        .ThenByDescending(h => h.From)
+                        .Select(history => new
+                        {
+                            history.From,
+                            To = history?.To?.ToString("yyyy-MM-ddTHH:mm:ss.fff") ?? "Hiện tại"
+                        }).ToList()
+                };
+
+                result.Add(workerResult);
+            }
+            return result;
+        }
+    }
+}
